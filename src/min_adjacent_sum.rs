47e06b6d2a74c637928dad6f8933507c2fac4128
chapter03.rs <@DenysKasyan>
2024-11-04 12:43:57 +0200

    practic_12

--- src/min_adjacent_sum.rs
+++ src/min_adjacent_sum.rs
@@ -1,33 +1,6 @@
 use rand::Rng;
 
 #[test]
-fn test_count_permutation() {
-    let shipments = vec![9, 3, 7, 2, 9];
-    println!("{}", calculate_moves(&shipments));
-}
-
-fn calculate_moves(shipments: &Vec<u32>) -> usize {
-    let total: u32 = shipments.iter().sum();
-    let count = shipments.len() as u32;
-
-    if total % count != 0 {
-        panic!("Неможливо розподілити рівномірно");
-    }
-
-    let average = total / count;
-    let mut total_moves = 0;
-    let mut current_balance: i32 = 0;
-
-    for &shipment in shipments {
-        current_balance += (shipment as i32) - (average as i32);
-        total_moves += current_balance.abs() as usize;
-    }
-
-    total_moves
-}
-
-
-#[test]
 fn test_min_sum() {
     let random_vec = create_random_vector(20);
     println!("{:?}", random_vec);
