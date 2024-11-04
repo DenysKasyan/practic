5976d662fe13f7065bfe84ee2ba0ca6a00689790
chapter03.rs <@DenysKasyan>
2024-11-04 10:28:17 +0200

    practic_09

---
+++ src/is_prime.rs
@@ -0,0 +1,29 @@
+#[test]
+fn test_is_prime() {
+    let test_data = [
+        (0, false),
+        (1, false),
+        (2, true),
+        (3, true),
+        (4, false),
+        (5, true),
+        (100, false),
+        (10007, true),
+    ];
+
+    for &(n, expected) in &test_data {
+        assert_eq!(is_prime(n), expected);
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    if n < 2 {
+        return false;
+    }
+    for i in 2..=((n as f64).sqrt() as u32) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
